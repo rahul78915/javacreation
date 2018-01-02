8b47d5c49950f291489481b52c8f78d9d4021bc5
rahul78915 <rahulprakash0862@gmail.com>
2018-01-02 12:55:37 +0530

    this is my first java project coding

---
+++ src/javacode/testcode.java
@@ -0,0 +1,18 @@
+package javacode;
+
+public class testcode {
+
+	public static void main(String[] args) {
+		int i=0;
+		switch(i)
+		{
+		case 0:
+			System.out.println("zero");
+			
+				
+		}
+		
+
+	}
+
+}
